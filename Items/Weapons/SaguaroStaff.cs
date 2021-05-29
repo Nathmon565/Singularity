436b8c894edfba7a5b6bcdd60d388d4f17d28013
Shogurt <jasonmomeyer8@gmail.com>
2021-05-28 22:51:54 -0700

    Add Saguaro Staff and corresponding sentry
    pre-hm desert sentry
    fires very slowly for moderate damage, but also does some contact damage
    also changed damage for snowman staff as it is obtainable fairly early on
    
    To do:
    Need sprite still, currently using the snowman ones

--- Items/Weapons/SaguaroStaff.cs
+++ Items/Weapons/SaguaroStaff.cs
@@ -7,18 +7,18 @@ using Terraria.ModLoader;
 namespace Singularity.Items.Weapons
 {
 
-	public class SnowmanStaff : ModItem
+	public class SaguaroStaff : ModItem
 	{
 		public override void SetStaticDefaults()
 		{
-			Tooltip.SetDefault("Summons a snowman to throw snowballs at your enemies \n\n'Ho ho hol' up'");
+			Tooltip.SetDefault("Summons a cactus to fire needles at your enemies \n\n'No touchy'");
 			ItemID.Sets.GamepadWholeScreenUseRange[item.type] = true;
 			ItemID.Sets.LockOnIgnoresCollision[item.type] = true;
 		}
 		
 		public override void SetDefaults()
 		{
-			item.damage = 14;
+			item.damage = 10;
 			item.sentry = true;
 			item.mana = 10; //How much mana this weapon takes to use.
 			item.width = 26; //Item width hitbox.
@@ -28,11 +28,11 @@ namespace Singularity.Items.Weapons
 			item.useStyle = ItemUseStyleID.SwingThrow;
 			item.summon = true;
 			item.noMelee = true; //Restricts this weapon dealing melee damage.
-			item.knockBack = 6;
+			item.knockBack = 9;
 			item.value = Item.buyPrice(0, 0, 50, 0); //How much this item is sold for.
 			item.rare = ItemRarityID.Green;
 			item.UseSound = SoundID.Item83;
-			item.shoot = ModContent.ProjectileType<SnowmanStaffSnowman>();
+			item.shoot = ModContent.ProjectileType<SaguaroStaffSaguaro>();
 		}
 
 		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
@@ -45,9 +45,9 @@ namespace Singularity.Items.Weapons
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(null, "IceCubes", 6);
-			recipe.AddIngredient(null, "Opal", 3);
-			recipe.AddIngredient(ItemID.SnowBlock, 100);
+			recipe.AddIngredient(ItemID.Amber, 6);
+			recipe.AddIngredient(ItemID.Cactus, 40);
+			recipe.AddIngredient(ItemID.AntlionMandible, 10);
 			recipe.AddTile(TileID.Anvils);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
