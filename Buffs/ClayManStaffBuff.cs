ac2a34bd6c0acdb40e31f92cccaeb542f08750dc
Shogurt <jasonmomeyer8@gmail.com>
2021-05-31 21:02:49 -0700

    Add Clay Man Staff
    Small clay dudes available veryearly pre-hm
    Currently not supposed to fly but don't dampen their spirits
    Supposed to have five per summon slot but only works if you have more than one minion slot available :/
    Need to add variants and finalize sprites for staff

---
+++ Buffs/ClayManStaffBuff.cs
@@ -0,0 +1,33 @@
+﻿using Singularity.Projectiles.Minions;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Singularity.Buffs
+{
+
+	public class ClayManStaffBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Clay Man");
+			Description.SetDefault("The Clay Men will fight for you");
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<ClayManStaffClayMan>()] > 0)
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+			else
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+	}
+}
---
+++ Items/Weapons/ClayManStaff.cs
@@ -0,0 +1,69 @@
+﻿using Singularity.Projectiles.Minions;
+using Singularity.Buffs;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Singularity.Items.Weapons
+{
+
+	public class ClayManStaff : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Clay Man Staff");
+			Tooltip.SetDefault("Summons Clay Men to fight for you");
+			ItemID.Sets.GamepadWholeScreenUseRange[item.type] = true; // This lets the player target anywhere on the whole screen while using a controller.
+			ItemID.Sets.LockOnIgnoresCollision[item.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 1;
+			item.knockBack = 3f;
+			item.mana = 10;
+			item.width = 32;
+			item.height = 32;
+			item.useTime = 36;
+			item.useAnimation = 36;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.value = Item.buyPrice(0, 0, 0, 50);
+			item.rare = ItemRarityID.Blue;
+			item.UseSound = SoundID.Item44;
+
+			// These below are needed for a minion weapon
+			item.noMelee = true;
+			item.summon = true;
+			item.buffType = ModContent.BuffType<ClayManStaffBuff>();
+			// No buffTime because otherwise the item tooltip would say something like "1 minute duration"
+			item.shoot = ModContent.ProjectileType<ClayManStaffClayMan>();
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			// This is needed so the buff that keeps your minion alive and allows you to despawn it properly applies
+			player.AddBuff(item.buffType, 2);
+			// Here you can change where the minion is spawned. Most vanilla minions spawn at the cursor position.
+			position = Main.MouseWorld;
+			return true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.CopperBar, 8);
+			recipe.AddIngredient(ItemID.ClayPot, 1);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+
+			ModRecipe recipe2 = new ModRecipe(mod);
+			recipe2.AddIngredient(ItemID.TinBar, 8);
+			recipe2.AddIngredient(ItemID.ClayPot, 1);
+			recipe2.AddTile(TileID.WorkBenches);
+			recipe2.SetResult(this);
+			recipe2.AddRecipe();
+		}
+	}
+}
