e50356ee2e714208f357e815e4f7b90ac711dbd5
Elias A Russell <47905784+GantherTumtums@users.noreply.github.com>
2021-06-05 14:16:26 -0700

    Add chest generation for glacial fragments

--- SingularityWorld.cs
+++ SingularityWorld.cs
@@ -9,6 +9,8 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.World.Generation;
 using static Terraria.ModLoader.ModContent;
+using Singularity.Items.Placeable;
+using Singularity.Items;
 
 namespace Singularity
 {
@@ -58,5 +60,22 @@ namespace Singularity
                 }
             }
         }
+        		public override void PostWorldGen() {
+			// Place items in ice chest
+			for (int chestIndex = 0; chestIndex < 1000; chestIndex++) {
+				Chest chest = Main.chest[chestIndex];
+				// If you look at the sprite for Chests by extracting Tiles_21.xnb, you'll see that the 12th chest is the Ice Chest. Since we are counting from 0, this is where 11 comes from. 36 comes from the width of each tile including padding. 
+				if (chest != null && Main.tile[chest.x, chest.y].type == TileID.Containers && Main.tile[chest.x, chest.y].frameX == 11 * 36) {
+					for (int inventoryIndex = 0; inventoryIndex < 40; inventoryIndex++) {
+						if (chest.item[inventoryIndex].type == ItemID.None) {
+							chest.item[inventoryIndex].SetDefaults(ModContent.ItemType<GlacialFragment>());
+                            chest.item[inventoryIndex].stack = WorldGen.genRand.Next(4, 6);
+							// Alternate approach: Random instead of cyclical: chest.item[inventoryIndex].SetDefaults(Main.rand.Next(itemsToPlaceInIceChests));
+							break;
+						}
+					}
+				}
+			}
+		}
     }
 }
