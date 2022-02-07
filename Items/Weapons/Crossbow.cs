773277518384b45c43bebcec96879338787c746b
Jason <jasonmomeyer8@gmail.com>
2022-02-06 17:55:45 -0700

    Finish Crossbow Functionality
    It does the charge n stuff
    Needs more polished loading sounds maybe
    You can still click 60 times to load it but who would do that honestly

--- Items/Weapons/Crossbow.cs
+++ Items/Weapons/Crossbow.cs
@@ -9,35 +9,88 @@ namespace Singularity.Items.Weapons
 	public class Crossbow : ModItem
 	{
 		public override void SetStaticDefaults() {
-			Tooltip.SetDefault("This is a modded gun.");
+			Tooltip.SetDefault("Takes a while to load.");
 		}
 
 		public override void SetDefaults() {
-			item.damage = 20; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+			item.damage = 30; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+			item.crit = 16;
 			item.ranged = true; // sets the damage type to ranged
 			item.width = 40; // hitbox width of the item
 			item.height = 20; // hitbox height of the item
-			item.useTime = 20; // The item's use time in ticks (60 ticks == 1 second.)
-			item.useAnimation = 20; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+			item.useTime = 1; // The item's use time in ticks (60 ticks == 1 second.)
+			item.useAnimation = 1; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+			//item.holdStyle = 1;
 			item.useStyle = ItemUseStyleID.HoldingOut; // how you use the item (swinging, holding out, etc)
 			item.noMelee = true; //so the item's animation doesn't do damage
 			item.knockBack = 4; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
 			item.value = 10000; // how much the item sells for (measured in copper)
 			item.rare = ItemRarityID.Green; // the color that the item's name will be in-game
-			//item.UseSound = SoundID.Item11; // The sound that this item plays when used.
-			item.autoReuse = false; // if you can hold click to automatically use it again
-			//item.shoot = 10; //idk why but all the guns in the vanilla source have this
+			//item.UseSound = SoundID.Item5; // The sound that this item plays when used.
+			item.autoReuse = true; // if you can hold click to automatically use it again
+			item.shoot = 10; //idk why but all the guns in the vanilla source have this
 			item.shootSpeed = 16f; // the speed of the projectile (measured in pixels per frame)
 			item.useAmmo = AmmoID.Arrow; // The "ammo Id" of the ammo item that this weapon uses. Note that this is not an item Id, but just a magic value.
+			item.channel = true;
 		}
-        float timer = 0;
-        public override bool CanUseItem(Player player) {
+        
+		public float timer = 0;
+
+		public override bool ConsumeAmmo (Player player){
+			if (timer != 58){
+				return false;
+			}
+			else{
+				return true;
+			}
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack){
+			//item.useAmmo = AmmoID.Arrow;
+			timer ++;
+            if (timer < 58){
+				item.autoReuse = true;
+				//item.useAmmo = AmmoID.None;
+			}
+			if (timer == 58){
+				item.UseSound = SoundID.Item17;
+			}
+			if (timer == 59){
+				player.moveSpeed += 0.5f;
+				item.UseSound = SoundID.Item5;
+				//item.useAmmo = AmmoID.Arrow;
+				item.autoReuse = false;
+			}
+			if (timer >= 60){
+				timer = 0;
+				item.UseSound = null;
+				return true;
+			}
+			return false;
+		}
+
+		/*void FindOwner(int whoAmI){
+			Player player = Main.player[item.owner];
+			player.silence = true;
+			if (player.channel){
+				player.statLife -= 10;
+				if (timer < 20f){
+					timer += 1f;
+				}	
+                if (timer >= 20f){
+					item.shoot = 10;
+					item.useAmmo = AmmoID.Arrow; // The "ammo Id" of the ammo item that this weapon uses. Note that this is not an item Id, but just a magic value.
+				}	
+			}
+		}*/
+		
+		/*public override bool CanUseItem(Player player) {
 				if (timer < 20f)
                 timer += 1f;
                 if (timer >= 20f)
                 item.shoot = 10;
 			return base.CanUseItem(player);
-		}
+		}*/
 		/*public override void AddRecipes() {
 			ModRecipe recipe = new ModRecipe(mod);
 			recipe.AddIngredient(ModContent.ItemType<ExampleItem>(), 10);
